911e28ac0729b08f56a2c928868f556f515fea46
Jacek Palczewski <jpalczewski@gmail.com>
2017-05-23 19:43:26 +0200

    fixed bug

--- src/Files/FileSavePartRequest.h
+++ src/Files/FileSavePartRequest.h
@@ -9,7 +9,7 @@
 struct FileSavePartRequest : public FilePartRequest
 {
     FileSavePartRequest(const AuthorKey &authorKey, const HASH_ARRAY &fileHash, size_t offset_, size_t size_)
-            : FilePartRequest(authorKey, fileHash, offset_, size_), bytes() {bytes.reserve(size_);}
+            : FilePartRequest(authorKey, fileHash, offset_, size_), bytes() {bytes.resize(size_, 0xDE);}
 
     std::vector<char> bytes;
 };
