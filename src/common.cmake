2d91345d54cdd8df95b2eab2affed814a8d2b563
jpalczewski <jpalczewski@gmail.com>
2017-06-04 02:09:08 +0200

    refactored compiling - now without horrible cmake files
    Signed-off-by: jpalczewski <jpalczewski@gmail.com>

--- src/common.cmake
+++ src/common.cmake
@@ -1,7 +1,6 @@
 set(COMMON
         ${simple_p2p_SOURCE_DIR}/src/CryptoUtils.cpp
         ${simple_p2p_SOURCE_DIR}/src/Dispatcher.cpp
-        ${simple_p2p_SOURCE_DIR}/src/examples.cpp
         ${simple_p2p_SOURCE_DIR}/src/Handler.cpp
         ${simple_p2p_SOURCE_DIR}/src/ConversionUtils.cpp
         )
